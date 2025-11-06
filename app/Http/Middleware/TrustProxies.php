4666afa40953c7f161c468067f92c0fc583395c0
roland-pixel <kharis7181@gmail.com>
2025-11-06 09:29:55 +0000

    first commit

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    protected $proxies = '*';
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
